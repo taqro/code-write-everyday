711d91fea3ff185bd57fc35d649af4b83b6aae40
taqro <takurousaitou94@yahoo.co.jp>
2022-01-15 10:19:20 +0900

    AtCoder

--- otoshidama.py
+++ otoshidama.py
@@ -1,83 +1,9 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "otoshidama.ipynb",
-      "provenance": [],
-      "authorship_tag": "ABX9TyM6cqGmvebPMMHc+4tbXbV5",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/taqro/code-write-everyday/blob/master/otoshidama.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": 8,
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "or6ywYi8JhNt",
-        "outputId": "95e14b0e-9aed-44fa-9894-8e0d24cb7ae3"
-      },
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "Nを入力（1≤N≤2000）\n",
-            "９\n",
-            "Yを入力（Yは1000 の倍数）\n",
-            "45000\n",
-            "[4, 1, 0]\n"
-          ]
-        }
-      ],
-      "source": [
-        "# 入力\n",
-        "print(\"Nを入力（1≤N≤2000）\")\n",
-        "N = int(input())\n",
-        "print(\"Yを入力（Yは1000 の倍数）\")\n",
-        "Y = int(input())\n",
-        "\n",
-        "# 金額の種類\n",
-        "a = [10000, 5000, 1000]\n",
-        "\n",
-        "result = [0] * 3\n",
-        "\n",
-        "for i in range(3):\n",
-        "  x = Y // a[i] #必要な枚数\n",
-        "  if x > N:\n",
-        "    x = N\n",
-        "  result[i] = x\n",
-        "  Y -= a[i] * result[i]\n",
-        "  N -= x\n",
-        "  if N <= 0:\n",
-        "    break\n",
-        "\n",
-        "# 合計金額がYにならない場合\n",
-        "if Y != 0:\n",
-        "  result = [-1, -1, -1]\n",
-        "\n",
-        "print(result)"
-      ]
-    }
-  ]
-}
+N, Y = map(int, input().split())
+
+ans = [-1, -1, -1]
+for i in range(N + 1):
+  for j in range(N + 1):
+    if (Y - (10000 * i + 5000 * j)) % 1000 == 0 and i + j + (Y - (10000 * i + 5000 * j)) // 1000 == N and (Y - (10000 * i + 5000 * j)) // 1000 >= 0:
+      ans = [i, j, (Y - (10000 * i + 5000 * j)) // 1000]
+
+print(str(ans[0]) + " " + str(ans[1]) + " " + str(ans[2]))
