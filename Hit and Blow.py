b63a2eca36fc6fed41268172d78cf3f88fd1c1ca
taqro <takurousaitou94@yahoo.co.jp>
2022-04-11 09:35:00 +0900

    AtCoder

---
+++ Hit and Blow.py
@@ -0,0 +1,19 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans_1 = 0
+for i in range(N):
+  if A[i] == B[i]:
+    ans_1 += 1
+
+ans_2 = 0
+for i in range(N):
+  for j in range(N):
+    if i == j:
+      continue
+    if A[i] == B[j]:
+      ans_2 += 1
+
+print(ans_1)
+print(ans_2)
