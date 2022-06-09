7bd592d9ee0b441237b0855f537fe39607316d87
taqro <takurousaitou94@yahoo.co.jp>
2022-06-09 10:20:26 +0900

    AtCoder

---
+++ Many Segments.py
@@ -0,0 +1,17 @@
+N = int(input())
+l = [0]*N
+r = [0]*N
+for i in range(N):
+    t,l[i],r[i] = map(int,input().split())
+    if t == 2:
+        r[i] -= 0.5
+    elif t == 3:
+        l[i] += 0.5
+    elif t == 4:
+        l[i] += 0.5
+        r[i] -= 0.5
+ans = 0
+for i in range(N):
+    for j in range(i+1,N):
+        ans += (max(l[i],l[j]) <= min(r[i],r[j]))
+print(ans)
